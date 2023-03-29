054c0cd077414fc0d459db9c1ca00f4910db3a80
Vinicius Jesus <92672912+viniciusantos2105@users.noreply.github.com>
2023-03-29 08:48:47 -0300

    Update WrongPasswordException.java

--- src/main/java/com/taskManager/exception/WrongPasswordException.java
+++ src/main/java/com/taskManager/exception/WrongPasswordException.java
@@ -3,6 +3,6 @@ package com.taskManager.exception;
 public class WrongPasswordException extends RuntimeException{
 
     public WrongPasswordException() {
-        super("Senha errada!!! Tente Novamente.");
+        super("Senha incorreta!!! Tente Novamente!  ");
     }
 }
